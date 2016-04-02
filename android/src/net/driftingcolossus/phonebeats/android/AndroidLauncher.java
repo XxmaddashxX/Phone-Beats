5510a6e7df203341c067aa92adf157fe2e799f35
XxmaddashxX <gcashmore96@hotmail.co.uk>
2016-04-02 11:53:37 +0100

    LibGDX Start
    Start of LibGDX project

---
+++ android/src/net/driftingcolossus/phonebeats/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.driftingcolossus.phonebeats.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.driftingcolossus.phonebeats.Phone Beats;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Phone Beats(), config);
+	}
+}
---
+++ desktop/src/net/driftingcolossus/phonebeats/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.driftingcolossus.phonebeats.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.driftingcolossus.phonebeats.Phone Beats;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Phone Beats(), config);
+	}
+}
